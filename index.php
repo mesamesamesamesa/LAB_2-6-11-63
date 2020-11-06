30e1c0eccb806627beab8fb326d386c9433fdf96
mesamesamesamesa <73683412+mesamesamesamesa@users.noreply.github.com>
2020-11-06 19:22:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,26 +10,22 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="col">
-                <img src="img/banner/banner2.jpg" class="img-fluid">
+            <div class="col-lg-4">
+                <img src="img/creative/creative8.jpg" alt="" height="100%" width="100%">
+                <h1>In town</h1>
+                <p1>when i go home</p1>
             </div>
-        </div> <!-- Close Row 1 -->
-        <div class="row">
-            <div class="col-sm-6">
-                <img src="img/work/work1.jpg" class="img-fluid rounded" />
-                <h1>Rounded</h1>
-                <p>Lorem ipsum dolor sit amet consectetur </p>
+            <div class="col-lg-4">
+                <img src="img/staffs/staff9.jpg" alt="" height="100%" width="100%">
+                <h1>my friend</h1>
+                <p1>his name is god</p1>
             </div>
-            <div class="col-sm-3">
-                <img src="img/work/work2.jpg" class="img-fluid img-thumbnail" />
-                <h1>Thumbnail</h1>
-                <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <div class="col-lg-4">
+                <img src="img/content/office3.jpg" alt="" height="100%" width="100%">
+                <h1>my office</h1>
+                <p1>so boring</p1>
             </div>
-            <div class="col-sm-3">
-                <img src="img/work/work3.jpg" class="img-fluid rounded-circle" />
-                <h1>Circle</h1>
-                <p>Lorem ipsum dolor sit amet consectetur</p>
-            </div>
+        </div>
         </div> <!-- Close Row 2 -->
     </div> <!-- Close container -->
 </body>
